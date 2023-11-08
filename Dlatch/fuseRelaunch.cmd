5693cb03833438b4cdda02acc8675108b94e6fde
FazelHaghighi <fazel.haghighi1399@gmail.com>
2023-11-09 00:29:52 +0330

    feat: add Dlatch code

---
+++ Dlatch/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "secureip" -o "/media/fazel/4600AC1600AC0ECD/Uni/CADS/codes/Dlatch/Dlatch_isim_beh.exe" -prj "/media/fazel/4600AC1600AC0ECD/Uni/CADS/codes/Dlatch/Dlatch_beh.prj" "work.Dlatch" 
